897fc70beb97a2e470862874848a5eb9d175e888
Arno Gorissen <arno.gorissen@student.ehb.be>
2020-05-24 18:52:44 +0200

    added comments and readme

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 'use strict'
-
 $(function () {
+
     console.log("pagina geladen");
 
 
-    //-------------alles inladen via json file ----------   
+    //-------------hier laden we alles dat we nodig hebben in via de json file ----------   
+
     $.ajax({
         method: "GET",
         url: 'entries.json',
@@ -12,43 +13,58 @@ $(function () {
         data: "data",
 
     }).done(function (data) {
+        /*met de for of loop gaan we voor elk element dat we binnenkrijgen de code uitvoeren die in de loop staat */
         for (let videos of data.items) {
-            console.log(videos);
-            // console.log(videos["link-to-video"]);
+            console.log(videos);              
+            /*we zetten de content op de pagina met de link naar de video en de juiste namen. 
+            Ook geef ik het genre en de category mee aan elke div als klasse om deze later te gebruiken*/
             $('#listofvideos').append(`
             <div class="videogallery ${videos.category} ${videos["genre-v2"]}">
-            <a href="${videos["link-to-video"].url}">
-              <img src="${videos.thumbnail.url}" alt="${videos.thumbnail.fileid}" >
+             <a href="${videos["link-to-video"].url}">
+               <img src="${videos.thumbnail.url}" alt="${videos.thumbnail.fileid}" >
             
-            <div class="desc"><h2>${videos.name}</h2>  <h3> ${videos.excerpt} </h3></div>
-            </a>
-            </div>
-        `);
+               <div class="desc">
+                 <h2>${videos.name}</h2> 
+                 <h3> ${videos.excerpt} </h3>
+               </div>
+             </a>
+            </div>`);
         }
-
-
     }).fail(function () {
         console.log("error");
     })
 
+
     //-------searchbar functie----------
+
+    /*de searchbar functie zoekt enkel in de klassen videogallery,
+     hierdoor zoekt deze in de volledige lijst ookal is er een filter geselecteerd, deze side effect heb ik niet kunnen oplossen.*/
     $("#searchBar").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
+        let value = $(this).val().toLowerCase();
         $("#listofvideos .videogallery").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
 
+
     //--------filters-----------
 
+    /* de manier waarop de knoppen werken is dat ze alleen de div elementen laten zien die overeenkomen met de class die ik heb opgegeven in de functie van die knop
+    de classes hebben ik meegegeven aan deze div elementen in mijn ajax call functie wanneer ik ze inlaadt op de pagina
+    dit werkt met de jquerry hide en show funties*/
+
+    /*de show all knop laat de volledige videogallery zien, ook laat deze de genre knoppen zien en verbergt deze de doelgroep knoppen
+    ik heb dit zo gedaan omdat de juiste content niet getoond werd op de pagina wanneer de doelgroep eerst geselecteerd werd 
+    (deze logica van knoppen verbergen heb ik ook toegepast bij de andere types knoppen) */
     $(document).on("click", "#showallbtn", function () {
         $('.videogallery').show();
         $('#genre').show();
         $('#doelgroep').hide();
-
-
     });
 
+
+    /*de doelgroep knoppen hiden de elementen met de class van de andere doelgroep. (bv de knop van volwassenen hide alle elementen met de class familie)
+    ook hiden deze knoppen de andere knoppen behalve de show all knop zodat alleen de juiste content weergegeven wordt. */
     $(document).on("click", "#volwassenbtn", function () {
         $('.familie').hide();
         $('#genre').hide();
@@ -63,6 +79,10 @@ $(function () {
 
     });
 
+    
+    /*de genre knoppen hiden eerst de volledige videogallery en showen dan de elementen met de geselecteerde class 
+    dit heeft wel als nadeel dat doordat we de volledige videogallery laten zien dat de filter van doelgroep zijn effect niet meer heeft
+    dit heb ik opgelost door de gebruiker eerst het genre te laten kiezen en pas daarna de doelgroep*/
     $(document).on("click", "#theaterbtn", function () {
         $('.videogallery').hide();
         $('.theater').show();
